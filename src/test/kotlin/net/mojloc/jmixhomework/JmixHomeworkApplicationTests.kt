b9d7742db9b0491cc795cce2c1eeb4e67c52ae47
Anatoliy Avramenko <mojloc@yandex.ru>
2022-10-06 17:40:42 +0700

    Start

---
+++ src/test/kotlin/net/mojloc/jmixhomework/JmixHomeworkApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.mojloc.jmixhomework
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class JmixHomeworkApplicationTests {
+
+    @Test
+    fun contextLoads() {
+
+    }
+}
