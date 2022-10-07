a57b8052f56113df8deb88bd1aafe34379fffce7
Anatoliy Avramenko <mojloc@yandex.ru>
2022-10-07 11:44:44 +0700

    Got Projects screen with possibility to choose employee as a member of the project team

--- src/main/kotlin/net/mojloc/jmixhomework/screen/project/ProjectEdit.kt
+++ src/main/kotlin/net/mojloc/jmixhomework/screen/project/ProjectEdit.kt
@@ -1,9 +1,48 @@
 package net.mojloc.jmixhomework.screen.project
 
+import io.jmix.ui.UiComponents
+import io.jmix.ui.component.CheckBox
+import io.jmix.ui.component.Component
+import io.jmix.ui.model.InstanceContainer
 import io.jmix.ui.screen.*
+import net.mojloc.jmixhomework.entity.Emplyee
 import net.mojloc.jmixhomework.entity.Project
+import org.springframework.beans.factory.annotation.Autowired
 
 @UiController("Project.edit")
 @UiDescriptor("project-edit.xml")
 @EditedEntityContainer("projectDc")
-class ProjectEdit : StandardEditor<Project>()
+class ProjectEdit : StandardEditor<Project>() {
+    @Autowired
+    private lateinit var uiComponents: UiComponents
+
+    @Autowired
+    private lateinit var projectDc: InstanceContainer<Project>
+
+
+    @Install(to = "emplyeesTable.isInProjectTeam", subject = "columnGenerator")
+    private fun emplyeesTableIsInProjectTeamColumnGenerator(emplyee: Emplyee?): Component {
+        var checkBox: CheckBox = uiComponents.create(CheckBox.NAME)
+        checkBox.value = isEmployeeInProjectTeam(emplyee)
+
+        checkBox.addValueChangeListener {
+            run {
+                if (isEmployeeInProjectTeam(emplyee) == true) {
+                    println("True!")
+                    projectDc.item.employees.remove(emplyee)
+                } else {
+                    println("False!")
+                    if (emplyee != null) {
+                        projectDc.item.employees.add(emplyee)
+                    }
+                }
+            }
+        }
+
+        return checkBox
+    }
+
+    private fun isEmployeeInProjectTeam (emplyee: Emplyee?): Boolean? {
+        return emplyee?.projects?.contains(projectDc.item)
+    }
+}
